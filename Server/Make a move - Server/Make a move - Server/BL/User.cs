0b55924802628f009c5cc24ec28e13b4a64fd721
MatanBarAsher <150009752+MatanBarAsher@users.noreply.github.com>
2024-04-20 11:43:49 +0300

    adding user controller

--- Server/Make a move - Server/Make a move - Server/BL/User.cs
+++ Server/Make a move - Server/Make a move - Server/BL/User.cs
@@ -6,6 +6,7 @@
         private string firstName;
         private string lastName;
         private string password;
+        private int gender;
         private string[] image;
         private int height;
         private DateTime birthday;
@@ -14,12 +15,13 @@
 
         public User() { }
 
-        public User(string email, string firstName, string lastName, string password, string[] image, int height, DateTime birthday, string phoneNumber, bool isActive)
+        public User(string email, string firstName, string lastName, string password, int gender, string[] image, int height, DateTime birthday, string phoneNumber, bool isActive)
         {
             this.email = email;
             this.firstName = firstName;
             this.lastName = lastName;
             this.password = password;
+            this.gender = gender;
             this.image = image;
             this.height = height;
             this.birthday = birthday;
@@ -31,6 +33,7 @@
         public string FirstName { get => firstName; set => firstName = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string Password { get => password; set => password = value; }
+        public int Gender { get => gender; set => gender = value; }
         public string[] Image { get => image; set => image = value; }
         public int Height { get => height; set => height = value; }
         public DateTime Birthday { get => birthday; set => birthday = value; }
