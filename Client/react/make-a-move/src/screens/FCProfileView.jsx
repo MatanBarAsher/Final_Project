c71386462fa1205b100f6c85945d5469371d0791
MatanBarAsher <150009752+MatanBarAsher@users.noreply.github.com>
2024-05-10 13:11:00 +0300

    profile screen

--- Client/react/make-a-move/src/screens/FCProfileView.jsx
+++ Client/react/make-a-move/src/screens/FCProfileView.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import FCHamburger from "../components/FCHamburger";
 import FCMatchScore from "../components/FCMatchScore";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import background from "../assets/images/Matan.jpg";
+import FCBackArrow from "../components/FCBackArrow";
 
 export default function FCProfileView({
   name,
@@ -13,54 +14,54 @@ export default function FCProfileView({
   interests,
   aboutMe,
 }) {
-  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
-
-  const toggleOverlay = () => {
-    setIsOverlayVisible(!isOverlayVisible);
-  };
   return (
-    <div className="profile-container">
-      <div
-        className="pBackground"
-        style={{
-          backgroundImage: `url(${background})`,
-        }}
-      >
-        <FCHamburger />
-      </div>
-      <div className="bottomP">
-        <FCMatchScore score={89} />
-        <h2>{name}</h2>
-        <p>
-          <b>גיל:</b>
-          {age}
-        </p>
-        <p>
-          <b>גובה:</b>
-          {height}
-        </p>
-        <p>
-          <b>מאיפה:</b>
-          {city}
-        </p>
-        <p>
-          <b>תחומי ענייו:</b>
-          {interests}
-        </p>
-        <p>
-          <b>על עצמי:</b>
-          <br />
-          {aboutMe}
-        </p>
-        <FavoriteBorderIcon
-          fontSize="large"
-          sx={{ color: "#ffffff", display: "" }}
-        />
-        <FavoriteIcon
-          fontSize="large"
-          sx={{ color: "#ffffff", display: "none" }}
-        />
-        {/* when viewed user is liked -> change display to "block" */}
+    <div className="overlay">
+      <div className="profile-container">
+        <div
+          className="pBackground"
+          style={{
+            backgroundImage: `url(${background})`,
+          }}
+        >
+          <FCHamburger />
+        </div>
+        <div className="bottomP">
+          <FCBackArrow color="white" />
+          <FCMatchScore score={89} />
+          <h2>{name}</h2>
+          <p>
+            <b>גיל: </b>
+            {age}
+          </p>
+          <p>
+            <b>גובה: </b>
+            {height}
+          </p>
+          <p>
+            <b>מאיפה: </b>
+            {city}
+          </p>
+          <p>
+            <b>תחומי עניין: </b>
+            {interests}
+            <br />
+            <br />
+          </p>
+          <p>
+            <b>על עצמי: </b>
+            <br />
+            {aboutMe}
+          </p>
+          <FavoriteBorderIcon
+            fontSize="large"
+            sx={{ color: "#ffffff", display: "", margin: 5 }}
+          />
+          <FavoriteIcon
+            fontSize="large"
+            sx={{ color: "#ffffff", display: "none" }}
+          />
+          {/* when viewed user is liked -> change display to "block" */}
+        </div>
       </div>
     </div>
   );
