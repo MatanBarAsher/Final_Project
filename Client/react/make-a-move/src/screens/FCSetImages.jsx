49afd27dc602ea71f1926adde0b269fc66f335da
MatanBarAsher <150009752+MatanBarAsher@users.noreply.github.com>
2024-05-12 17:18:46 +0300

    start the img input

--- Client/react/make-a-move/src/screens/FCSetImages.jsx
+++ Client/react/make-a-move/src/screens/FCSetImages.jsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import FCCustomBtn from "../components/FCCustomBtn";
 import { makeAmoveUserServer } from "../services";
 import { useNavigate } from "react-router-dom";
+import FCImageInp from "../components/FCImageInp";
 
 export default function FCSetImages() {
   return (
-    <input
-      type="file"
-      placeholder="Image"
-      name="image"
-      onChange={handleChange}
-      value={data.image}
-      className="input"
-      accept=".jpg, .jpeg"
-      required
-    />
+    <>
+      <h1>תמונות</h1>
+      <div className="images-inp-container">
+        <FCImageInp />
+        <FCImageInp />
+        <FCImageInp />
+        <FCImageInp />
+        <FCImageInp />
+        <FCImageInp />
+      </div>
+      <FCCustomBtn title="סיום" />
+    </>
   );
 }
