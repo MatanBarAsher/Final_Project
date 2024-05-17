97268ced58441ffaced358eaa70c8922141ef870
MatanBarAsher <150009752+MatanBarAsher@users.noreply.github.com>
2024-05-17 15:47:37 +0300

    fixing DB services mess

--- Server/Make a move - Server/Make a move - Server/DAL/DBservicesUser.cs
+++ Server/Make a move - Server/Make a move - Server/DAL/DBservicesUser.cs
@@ -648,7 +648,6 @@ namespace Make_a_move___Server.DAL
 
 
 
-
         public User GetUserByEmail(string email)
         {
             SqlConnection con;
@@ -684,38 +683,12 @@ namespace Make_a_move___Server.DAL
 
                 return u;
             }
-            catch (Exception ex)
+             catch (Exception ex)
             {
                 // write to log
                 throw (ex);
-                // Create the connection
-                con = connect("myProjDB");
-            }
-            catch (Exception ex)
-            {
-                // Write to log
-                throw ex;
             }
 
-
-        }
-
-        public int ChangeUserImages(string email, string[] images)
-        {
-            
-            cmd = CreateChangeUserImagesCommandWithStoredProcedure("SP_ChangeUserImages", con, email, images); // Create the command
-
-            try
-            {
-                //Execute the command
-                int numEffected = cmd.ExecuteNonQuery();
-                return numEffected;
-            }
-            catch (Exception ex)
-            {
-                // Write to log
-                throw ex;
-            }
             finally
             {
                 if (con != null)
@@ -724,20 +697,73 @@ namespace Make_a_move___Server.DAL
                     con.Close();
                 }
             }
-        }
 
+        }
         private SqlCommand CreateSelectUserByEmailCommand(String spName, SqlConnection con, string email)
         {
+
             SqlCommand cmd = new SqlCommand(); // create the command object
 
-            // set up the command properties like command text, timeout, etc.
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
 
             cmd.Parameters.AddWithValue("@inputEmail", email); // Add parameter for email
+
+            return cmd;
+
+        }
+
+
+
+        public int ChangeUserImages(string email, string[] images)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+
+            try
+            {
+                con = connect("myProjDB"); // create the connection
+            }
+            catch (Exception ex)
+            {
+                // write to log
+                throw (ex);
+            }
+
+            cmd = CreateChangeUserImagesCommandWithStoredProcedure("SP_ChangeUserImages", con, email, images); // Create the command
+
+                try
+                {
+                    //Execute the command
+                    int numEffected = cmd.ExecuteNonQuery();
+                    return numEffected;
+                }
+                catch (Exception ex)
+                {
+                    // Write to log
+                    throw ex;
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        // close the db connection
+                        con.Close();
+                    }
+                }
+        }
+
+       
         //---------------------------------------------------------------------------------
         // Create the SqlCommand for changing user images using a stored procedure
         //---------------------------------------------------------------------------------
 
-        private SqlCommand CreateChangeUserImagesCommandWithStoredProcedure(string spName, SqlConnection con, string email, string[] images)
+        private SqlCommand CreateChangeUserImagesCommandWithStoredProcedure(String spName, SqlConnection con, string email, string[] images)
         {
             SqlCommand cmd = new SqlCommand(); // Create the command object
 
