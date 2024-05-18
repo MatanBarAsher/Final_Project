34144f694093c8ea3dff88d3e01913bd9a730e0e
YaelTS <terneryael@gmail.com>
2024-05-18 15:27:26 +0300

    update genser and consolLog

--- Client/react/make-a-move/src/screens/FCLocation.jsx
+++ Client/react/make-a-move/src/screens/FCLocation.jsx
@@ -3,25 +3,49 @@ import FCCustomBtn from "../components/FCCustomBtn";
 import logo from "../assets/images/Logo.png";
 import FCHamburger from "../components/FCHamburger";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+// import { useNavigate } from "react-router-dom";
 
 export default function FCLocation() {
-  const [value, setValue] = useState(null);
-  return (
-    <>
-      <FCHamburger />
-      <img src={logo} className="logoSM" />
-      <h1>אישור מיקום:</h1>
-      <div>
-        <GooglePlacesAutocomplete
-          selectProps={{
-            value,
-            onChange: setValue,
-          }}
-          apiKey={"AIzaSyDfO7S5c0OcZki3aQEBN1xMrDj4qD9v8Uk"}
-          debounce={300}
+  // const navigate = useNavigate("");
+  const [locationValue, setLocationValue] = useState("");
+
+  // const handleLocationChange = (e) => {
+  //   // setLocationValue((prev) => ({ ...prev, ["currentLocation"]: e }));
+  //   setLocationValue("currentLocation", e);
+  // };
+
+  const handleSubmit = () => {
+    //go to server with locationValue as prop
+    makeAmoveUserServer.setLocationValue(locationValue).then((response) => {
+      if (response) {
+        console.log("success");
+        console.log(response);
+        // navigate("/profile");
+      } else {
+        console.log("failure");
+      }
+    });
+    return (
+      <>
+        <FCHamburger />
+        <img src={logo} className="logoSM" />
+        <h1>אישור מיקום:</h1>
+        <div>
+          <GooglePlacesAutocomplete
+            selectProps={{
+              value,
+              onChange: setValue,
+            }}
+            apiKey={"AIzaSyDfO7S5c0OcZki3aQEBN1xMrDj4qD9v8Uk"}
+            debounce={300}
+          />
+        </div>
+        <FCCustomBtn
+          title="אישור"
+          mt={"130px"}
+          onClick={() => handleSubmit(value)}
         />
-      </div>
-      <FCCustomBtn title="אישור" mt={"130px"} />
-    </>
-  );
+      </>
+    );
+  };
 }
