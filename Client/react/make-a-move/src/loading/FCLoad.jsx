1d360695a0f06379a7e0c48c5cd328ca82cb07a4
YaelTS <terneryael@gmail.com>
2024-05-26 15:57:46 +0300

    color loading

--- Client/react/make-a-move/src/loading/FCLoad.jsx
+++ Client/react/make-a-move/src/loading/FCLoad.jsx
@@ -4,9 +4,7 @@ import React from "react";
 export const FCLoad = () => {
   return (
     <div>
-      
-      <ReactLoading type="spin" color="#0000FF" height={100} width={50} />
-
+      <ReactLoading type="spin" color="#efe1d1" height={100} width={50} />
     </div>
   );
 };
